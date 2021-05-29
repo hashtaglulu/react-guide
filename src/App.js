ac6de37f28fe7706583885b8823fd8b1b5aa113d
Laura <n.laura96@yahoo.com>
2021-05-29 23:47:51 +0300

    first change

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div >
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Cevaaaaaa
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ExpenseItem></ExpenseItem>
     </div>
   );
 }
